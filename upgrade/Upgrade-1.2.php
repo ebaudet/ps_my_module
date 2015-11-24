531b68e4e4d32952cbd9ae968b09c4f6dd30c116
ebaudet <ebaudet@202-ecommerce.com>
2015-11-24 16:36:55 +0100

    for the module's upgrade

---
+++ upgrade/Upgrade-1.2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 24/11/2015
+ * Time: 15:46
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_1_2($module)
+{
+    // Process Module upgrade to 1.2
+    // ....
+    return true; // Return true if success.
+}
